b70d4e01187992dee227856c1e0814b778220f6a
diva.miet.cse <diva.miet.cse@gmail.com>
2017-06-06 22:28:32 +0530

    Comparison with Random Forest Algorithm

--- ISLR/src/main/R/ch-1_2/knnUsingCaretPkg.R
+++ ISLR/src/main/R/ch-1_2/knnUsingCaretPkg.R
@@ -15,8 +15,8 @@ idxtrain = createDataPartition(y = prc$diagnosis_result,
                                p = 0.70,
                                list = FALSE)
 
-train_data = prc[idxtrain,]
-test_data = prc[-idxtrain,]
+train_data = prc[idxtrain, ]
+test_data = prc[-idxtrain, ]
 
 prop.table(table(train_data$diagnosis_result))
 prop.table(table(test_data$diagnosis_result))
@@ -43,17 +43,23 @@ model
 plot(model)
 
 #' Predict
-prediction=predict(model,newdata = test_data)
+prediction = predict(model, newdata = test_data)
 
-confusionMatrix(prediction,test_data$diagnosis_result)
+confusionMatrix(prediction, test_data$diagnosis_result)
 
-mean(prediction==test_data$diagnosis_result)
+mean(prediction == test_data$diagnosis_result)
 
 
 
 
 #'#Now verifying 2 class summary function
-ctrl <- trainControl(method="repeatedcv",repeats = 3,classProbs=TRUE,summaryFunction = twoClassSummary)
+ctrl <-
+  trainControl(
+    method = "repeatedcv",
+    repeats = 3,
+    classProbs = TRUE,
+    summaryFunction = twoClassSummary
+  )
 model = train(
   diagnosis_result ~ .,
   data = train_data,
@@ -65,22 +71,44 @@ model = train(
 
 model
 
-plot(model,prin.thres=0.5,type="S")
+plot(model, prin.thres = 0.5, type = "S")
 
-prediction=predict(model,newdata = test_data)
+prediction = predict(model, newdata = test_data)
+
+confusionMatrix(prediction, test_data$diagnosis_result)
+
+mean(prediction == test_data$diagnosis_result)
+
+#' #' Plot ROc curve
+#' library(pROC)
+#'
+#' prediction=predict(model,newdata = test_data,type = "prob")
+#'
+#' knnROC=roc(test_data$diagnosis_result,prediction,levels=rev(test_data$diagnosis_result))
+#' knnROC
+#'
+#' plot(knnROC, type="S", print.thres= 0.5)
+
+#' Applying Random Forest to see the Performance diffrence
+ctrl <- trainControl(method = "repeatedcv", repeats = 3)
+rfModel <-
+  train(
+    diagnosis_result ~ .,
+    data = train_data,
+    method = "rf",
+    trControl = ctrl,
+    preProcess = c("center", "scale"),
+    tuneLength = 20
+  )
+
+rfModel
+
+plot(rfModel)
+
+prediction=predict(rfModel,newdata = test_data)
 
 confusionMatrix(prediction,test_data$diagnosis_result)
 
 mean(prediction==test_data$diagnosis_result)
-
-#' #' Plot ROc curve
-#' library(pROC)
-#' 
-#' prediction=predict(model,newdata = test_data,type = "prob")
-#' 
-#' knnROC=roc(test_data$diagnosis_result,prediction,levels=rev(test_data$diagnosis_result))
-#' knnROC
-#' 
-#' plot(knnROC, type="S", print.thres= 0.5)
 
 
